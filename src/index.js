211fe8e86965cd52a6c86ab0847c54d9ce8590ec
Xinyang Zhang <xinyzhang9@gmail.com>
2020-09-27 00:54:07 +0800

    read logs in threads

--- src/index.js
+++ src/index.js
@@ -1,1 +1,107 @@
-console.log('hello')
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads')
+const os = require('os')
+const fs = require('fs')
+const path = require('path')
+const readline = require('readline')
+const threadCount = os.cpus().length
+const threads = new Set()
+const N = 10000
+const LOG_REL_PATH = '../var/log/httpd'
+const logsToDispatch = new Array(threadCount)
+for(let i = 0; i < logsToDispatch.length; i++) {
+    logsToDispatch[i] = []
+}
+
+const hash = (str) => {
+    return str.substring(str.length-6, str.length-4) % threadCount
+}
+
+const merge = (arr1, arr2) => {
+    return arr1.map((v,i) => v+arr2[i])
+}
+
+const getPercent = (preSum, percent) => {
+    console.log(preSum[preSum.length-1])
+    const target = Math.floor(preSum[preSum.length-1] * percent)
+    let l = 0, r = preSum.length-1
+    while(l <= r) {
+        let mid = l + Math.floor((r-l)/2)
+        if(preSum[mid] < target) {
+            l = mid + 1
+        } else {
+            r = mid-1
+        }
+    }
+    return l
+}
+
+const getPreSum = (arr) => {
+    let preSum = 0
+    return arr.map((v,i) => {
+        let res = v + preSum
+        preSum += v
+        return res
+    })
+}
+
+if(isMainThread) {
+    let counters = new Array(N).fill(0)
+    fs.readdir(path.resolve(__dirname, LOG_REL_PATH), (err, files) => {
+        if(err) console.error(err)
+        
+        files.forEach(f => {
+            if(!f.endsWith('.log')) return
+            logsToDispatch[hash(f)].push(f)
+        })
+        // create workers
+        for(let i = 0; i < threadCount; i++) {
+            threads.add(new Worker(__filename, { workerData: { logs:logsToDispatch[i] } }))
+        }
+        for(let worker of threads) {
+            worker.on('error', (err) => {throw err })
+            worker.on('exit', () => {
+                threads.delete(worker)
+                // console.log(`Thread exiting, ${threads.size} running...`)
+                if(threads.size === 0) {
+                    const preSum = getPreSum(counters)
+                    const res90 = getPercent(preSum, 0.9)
+                    const res95 = getPercent(preSum, 0.95)
+                    const res99 = getPercent(preSum, 0.99)
+                    console.log(`90% of requests return a response within ${res90} ms`)
+                    console.log(`95% of requests return a response within ${res95} ms`)
+                    console.log(`99% of requests return a response within ${res99} ms`)
+                }
+            })
+            worker.on('message', (data) => {
+                // merge data
+                counters = merge(counters, data)
+                
+            })
+        }
+    })
+} else {
+    const threadRes = new Array(N).fill(0)
+    workerData.logs.forEach((f, i) => {
+        let filePath = path.resolve(__dirname, LOG_REL_PATH, f)
+            const rl = readline.createInterface({
+                input: fs.createReadStream(filePath)
+            })
+
+            rl.on('line', (line) => {
+                let arr = line.split(' ')
+                let respondTime = arr[arr.length-1]
+                if(parseInt(respondTime) > 0) {
+                    threadRes[respondTime]++
+                }
+            })
+
+            rl.on('close', () => {
+                if(i === workerData.logs.length-1) {
+                    console.log(threadRes)
+                    parentPort.postMessage(threadRes)
+                }
+            })
+
+    })
+    
+}
--- src/logGenerator.js
+++ src/logGenerator.js
@@ -2,16 +2,34 @@ const fs = require('fs')
 const path = require('path')
 const moment = require('moment')
 const logPath = path.resolve(__dirname,'../var/log/httpd')
-const _today = moment(new Date())
+
+
+
+const getNumberInNormalDistribution = (mean,std_dev) => {
+    return mean+(randomNormalDistribution()*std_dev) 
+}
+
+const randomNormalDistribution = () => {
+    let u=0.0, v=0.0, w=0.0, c=0.0
+    do {
+        u=Math.random()*2-1.0
+        v=Math.random()*2-1.0
+        w=u*u+v*v
+    } while(w==0.0||w>=1.0)
+    c = Math.sqrt((-2*Math.log(w))/w)
+    return u*c
+}
+
+
 const gen = (num) => {
     for(let i = 0; i < num; i++) {
-        const fileName = _today.subtract(i,'days').format('YYYY-MM-DD')+'.log'
+        const fileName = moment(new Date()).subtract(i,'days').format('YYYY-MM-DD')+'.log'
         let content = ''
-        const lines = Math.floor(Math.random() * 1000 + 300)
+        const lines = 10000
         for(let i = 0; i < lines; i++) {
-            const time = _today.add(i,'seconds').add(10 * i,'milliseconds').format('YYYY/MM/DD:HH:mm:ss')
+            const time = moment(new Date()).add(i,'seconds').add(10 * i,'milliseconds').format('YYYY/MM/DD:HH:mm:ss')
             const ip = `10.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}`
-            content += `${ip} [${time}] GET /api/playeritems?playerId=${Math.floor(Math.random() * 10000)} 200 ${Math.floor(Math.random() * 10000)}\n`
+            content += `${ip} [${time}] GET /api/playeritems?playerId=${Math.floor(Math.random() * 10000)} 200 ${Math.floor(getNumberInNormalDistribution(500,200))}\n`
         }
         fs.writeFileSync(path.resolve(logPath, fileName),content,(err) => {
             if(err) {
@@ -19,8 +37,7 @@ const gen = (num) => {
             }
         })
     }
-    console.log('write to logs successfully')
+    console.log(`write to ${num} logs successfully`)
 }
 
-
-gen(100)
+gen(30)
